46aa85ba2e49b367a800b3692ec98479efe70d61
Mayur Kulkarni <kulkarni.mayur@gmail.com>
2021-03-02 16:56:07 +0530

    Added unit testcases

---
+++ backend/end_users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from end_users.models import Product, Order, OrderItem
+
+# Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(Order)
